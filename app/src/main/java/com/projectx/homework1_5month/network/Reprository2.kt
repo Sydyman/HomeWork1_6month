2e0ae2f00de22a873c5542c7b2bb0ece821a1402
Sydyman <Homelandertexas@gmail.com>
2024-10-14 21:35:37 +0600

    д3 4

--- app/src/main/java/com/projectx/homework1_5month/network/Reprository2.kt
+++ app/src/main/java/com/projectx/homework1_5month/network/Reprository2.kt
@@ -4,29 +4,38 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.projectx.homework1_5month.api.ApiService
 import com.projectx.homework1_5month.models.Character
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.projectx.resource.Resource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class Reprository2 @Inject constructor(private val api: ApiService) {
+class Reprository2 @Inject constructor(private val api: ApiService,
+                                       private val viewModelScope: CoroutineScope
+) {
 
-    fun getCharacterById(id: Int): MutableLiveData<Character?> {
-        val data = MutableLiveData<Character?>()
 
-        api.fetchCharacterById(id).enqueue(object : Callback<Character> {
-            override fun onResponse(call: Call<Character>, response: Response<Character>) {
-                if (response.isSuccessful) {
-                    data.postValue(response.body())
-                } else {
-                    data.postValue(null)
+    fun getCharacterById(id: Int): LiveData<Resource<Character?>> {
+        val data = MutableLiveData<Resource<Character?>>()
+        data.postValue(Resource.Loading())
+
+
+        viewModelScope.launch {
+            try {
+                val character = withContext(Dispatchers.IO) {
+                    api.fetchCharacterById(id)
                 }
-            }
 
-            override fun onFailure(call: Call<Character>, t: Throwable) {
-                data.postValue(null)
+                if (character != null) {
+
+                } else {
+                    data.postValue(Resource.Error("Character not found"))
+                }
+            } catch (t: Throwable) {
+                data.postValue(Resource.Error(t.localizedMessage ?: "Unknown Error"))
             }
-        })
+        }
         return data
     }
 }
